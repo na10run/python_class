8eda4949c3472f9ad7d226e7f17a62ff707bf382
na10run <na10run@gmail.com>
2015-08-25 18:32:54 +0545

    first update

--- na_RUN.py
+++ na_RUN.py
@@ -1,20 +1,29 @@
 #  Assignment 1:
 
 test_word = {'to': 0, 'her': 0, 'rabbit': 0, 'she': 0, 'adventures': 0, 'alice': 0, 'by': 0, 'in': 0}
-text_file = open('pg11.txt', 'r')
 
-# now we loop through each line
-for line in text_file:
 
-    for word in line.split():
-        word = word.replace(",", "")
-        word = word.replace(".", "")
-        word = word.replace("!", "")
-        word = word.replace("?", "")
+def count_word():
+    text_file = open('pg11.txt', 'r')
+    for line in text_file:
 
-        for keys, values in test_word.items():
+        for word in line.split():
+            word = word.replace(",", "")
+            word = word.replace(".", "")
+            word = word.replace("!", "")
+            word = word.replace("?", "")
 
-            if keys == word.lower():
-                test_word[keys] += 1
+            for keys, values in test_word.items():
 
-print(test_word)
+                if keys == word.lower():
+                    test_word[keys] += 1
+
+    print(test_word)
+
+
+def sort_data():
+    test_word.sort()
+    print(test_word)
+
+count_word()
+sort_data()
